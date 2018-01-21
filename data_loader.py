24b021ec3b8cf9bae951e9cc45cd53bf17323871
Joe Sanford <josephsanford@gmail.com>
2018-01-21 10:30:28 -0500

    PEP8

--- data_loader.py
+++ data_loader.py
@@ -20,7 +20,7 @@ def get_data():
         for type in types:
             data[language][type] = {}
             for state in states:
-                time.sleep(2) # to avoid GitHub Search API Rate Limits: https://developer.github.com/v3/search/#rate-limit
+                time.sleep(2)  # to avoid GitHub Search API Rate Limits: https://developer.github.com/v3/search/#rate-limit
                 r = requests.get(base_url.format(language=language, type=type, state=state),
                                  auth=HTTPBasicAuth(github_username, github_token))
                 data[language][type][state] = r.json().get('total_count', r.text)
@@ -29,4 +29,15 @@ def get_data():
 
 
 def get_mocked_data():
-    return {'python': {'pr': {'open': 159723, 'closed': 3079050}, 'issue': {'open': 1528487, 'closed': 2919859}}, 'javascript': {'pr': {'open': 539227, 'closed': 5790580}, 'issue': {'open': 2361186, 'closed': 3950643}}, 'css': {'pr': {'open': 57471, 'closed': 872585}, 'issue': {'open': 248292, 'closed': 460819}}, 'ruby': {'pr': {'open': 521155, 'closed': 2280152}, 'issue': {'open': 369578, 'closed': 977661}}, 'java': {'pr': {'open': 139682, 'closed': 2916285}, 'issue': {'open': 2185928, 'closed': 4174197}}, 'go': {'pr': {'open': 26750, 'closed': 762512}, 'issue': {'open': 193987, 'closed': 476020}}, 'php': {'pr': {'open': 90126, 'closed': 1707521}, 'issue': {'open': 756084, 'closed': 2101087}}, 'c': {'pr': {'open': 47574, 'closed': 725236}, 'issue': {'open': 1133153, 'closed': 1613625}}, 'c++': {'pr': {'open': 47574, 'closed': 725236}, 'issue': {'open': 1133153, 'closed': 1613625}}, 'swift': {'pr': {'open': 25325, 'closed': 216524}, 'issue': {'open': 66887, 'closed': 137565}}, 'shell': {'pr': {'open': 34215, 'closed': 552111}, 'issue': {'open': 178617, 'closed': 393265}}, 'objective-c': {'pr': {'open': 23845, 'closed': 253407}, 'issue': {'open': 288751, 'closed': 501042}}}
+    return {'python': {'pr': {'open': 159723, 'closed': 3079050}, 'issue': {'open': 1528487, 'closed': 2919859}},
+            'javascript': {'pr': {'open': 539227, 'closed': 5790580}, 'issue': {'open': 2361186, 'closed': 3950643}},
+            'css': {'pr': {'open': 57471, 'closed': 872585}, 'issue': {'open': 248292, 'closed': 460819}},
+            'ruby': {'pr': {'open': 521155, 'closed': 2280152}, 'issue': {'open': 369578, 'closed': 977661}},
+            'java': {'pr': {'open': 139682, 'closed': 2916285}, 'issue': {'open': 2185928, 'closed': 4174197}},
+            'go': {'pr': {'open': 26750, 'closed': 762512}, 'issue': {'open': 193987, 'closed': 476020}},
+            'php': {'pr': {'open': 90126, 'closed': 1707521}, 'issue': {'open': 756084, 'closed': 2101087}},
+            'c': {'pr': {'open': 47574, 'closed': 725236}, 'issue': {'open': 1133153, 'closed': 1613625}},
+            'c++': {'pr': {'open': 47574, 'closed': 725236}, 'issue': {'open': 1133153, 'closed': 1613625}},
+            'swift': {'pr': {'open': 25325, 'closed': 216524}, 'issue': {'open': 66887, 'closed': 137565}},
+            'shell': {'pr': {'open': 34215, 'closed': 552111}, 'issue': {'open': 178617, 'closed': 393265}},
+            'objective-c': {'pr': {'open': 23845, 'closed': 253407}, 'issue': {'open': 288751, 'closed': 501042}}}
--- devs_are_lazy.py
+++ devs_are_lazy.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+
 from data_loader import get_mocked_data, get_data, languages
 
 app = Flask(__name__)
@@ -11,17 +12,22 @@ Bootstrap(app)
 @app.route('/')
 def index():
     github_data = get_mocked_data()
-    open_pull_requests, closed_pull_requests, pull_request_ratios, open_issues, closed_issues, issue_ratios = ([] for _ in range(6))
+    open_pull_requests, closed_pull_requests, pull_request_ratios, open_issues, closed_issues, issue_ratios = ([] for _
+                                                                                                               in
+                                                                                                               range(6))
 
     for language in github_data:
         open_pull_requests.append(github_data[language]['pr']['open'])
         closed_pull_requests.append(github_data[language]['pr']['closed'])
-        pull_request_ratios.append(int(github_data[language]['pr']['open'])/float(github_data[language]['pr']['closed']))
+        pull_request_ratios.append(int(github_data[language]['pr']['open']) / float(github_data[language]['pr']['closed']))
         open_issues.append(github_data[language]['issue']['open'])
         closed_issues.append(github_data[language]['issue']['closed'])
-        issue_ratios.append(int(github_data[language]['issue']['open'])/float(github_data[language]['issue']['closed']))
+        issue_ratios.append(int(github_data[language]['issue']['open']) / float(github_data[language]['issue']['closed']))
 
-    return render_template('index.html', open_pull_requests=open_pull_requests, closed_pull_requests=closed_pull_requests, pull_request_ratios=pull_request_ratios, open_issues=open_issues, closed_issues=closed_issues, issue_ratios=issue_ratios, labels=languages)
+    return render_template('index.html', open_pull_requests=open_pull_requests,
+                           closed_pull_requests=closed_pull_requests, pull_request_ratios=pull_request_ratios,
+                           open_issues=open_issues, closed_issues=closed_issues, issue_ratios=issue_ratios,
+                           labels=languages)
 
 
 if __name__ == '__main__':
